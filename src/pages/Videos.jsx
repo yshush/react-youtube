9dae20f4a38740afd82841adc4a235e83b06cc5c
양성훈 <tjdgns7533@gmail.com>
2022-12-07 23:45:06 +0900

    complete

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -11,11 +11,12 @@ export default function Videos() {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], () => youtube.search(keyword));
+  } = useQuery(["videos", keyword], () => youtube.search(keyword), {
+    staleTime: 1000 * 60 * 1,
+  });
 
   return (
     <>
-      <div>Videos {keyword ? `🔍${keyword}` : "🔥"}</div>
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong 😖</p>}
       {videos && (
